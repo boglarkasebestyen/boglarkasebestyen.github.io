cdf262e8d018051417c23c3604aa81301a487893
Boglárka Sebestyén <sebestyenboglarkacsilla@gmail.com>
2021-08-25 19:19:25 -0700

    Minor modifications on projects page and styling.

--- docs/busTracker/mapanimation.js
+++ docs/busTracker/mapanimation.js
@@ -4,8 +4,8 @@ let map = null;
 function setupMap() {
     map = new mapboxgl.Map({
         accessToken: "pk.eyJ1IjoiYi1zZWJlIiwiYSI6ImNrczg0bnM4NTBscjYybm8yMWVqYWp3Y2kifQ.6ZbuMId34XsGbDapIiqi2Q",
-        container: 'map',
-        style: 'mapbox://styles/mapbox/satellite-streets-v11',
+        container: "map",
+        style: "mapbox://styles/mapbox/navigation-day-v1",
         center: [-71.091542,42.358862],
         zoom: 12
     });
@@ -23,7 +23,7 @@ function findBus(id, busses) {
 
 // request bus data from MBTA
 async function getBusLocations() {
-    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+    const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
     const response = await fetch(url);
     const json = await response.json();
     return json.data;
@@ -44,7 +44,7 @@ async function run() {
             busRecord.marker.setLngLat([bus.attributes.longitude, bus.attributes.latitude]);
         } else {
             console.log("Adding new marker for " + bus.id);
-            var el = document.createElement('div');
+            var el = document.createElement("div");
             el.className = "customMarker";
             var marker = new mapboxgl.Marker(el).setLngLat([bus.attributes.longitude, bus.attributes.latitude]).addTo(map);
             busses.push({
