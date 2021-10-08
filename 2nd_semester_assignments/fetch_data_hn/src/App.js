29e85fdf36918ebaaf92c6c65a54564026b98c3d
Boglárka Sebestyén <sebestyenboglarkacsilla@gmail.com>
2021-10-07 20:27:34 -0700

    Hacker News fetch data exercise.

---
+++ 2nd_semester_assignments/fetch_data_hn/src/App.js
@@ -0,0 +1,150 @@
+import React, {useState, useEffect, useReducer, Fragment} from "react";
+import Bootstrap from "react-bootstrap";
+import Axios from "axios";
+import "./index.css"
+
+const Pagination = ({ items, pageSize, onPageChange }) => {
+  if (items.length <= 1) return null;
+
+  let num = Math.ceil(items.length / pageSize);
+  let pages = range(1, num + 1);
+  const list = pages.map(page => {
+    return (
+      <button type="button" className="btn btn-info m-1" key={page} onClick={onPageChange}>
+        {page}
+      </button>
+    );
+  });
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+};
+const range = (start, end) => {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((item, i) => start + i);
+};
+function paginate(items, pageNumber, pageSize) {
+  const start = (pageNumber - 1) * pageSize;
+  let page = items.slice(start, start + pageSize);
+  return page;
+}
+const useDataApi = (initialUrl, initialData) => {
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await Axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
+};
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
+// App that gets data from Hacker News url
+function App() {
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("MIT");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=MIT",
+    {
+      hits: []
+    }
+  );
+  const handlePageChange = e => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="button" className="btn btn-info m-1">Search</button>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {page.map(item => (
+            <li class="list-group-item" key={item.objectID}>
+              <a class="list-group-item" href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Pagination
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      ></Pagination>
+    </form>
+    </Fragment>
+
+  );
+}
+
+export default App;
